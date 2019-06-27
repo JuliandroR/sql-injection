e6053eab37bf3ca84e548b698c556bf45020fec7
JuliandroR <juliandro.ribeiro@novaandradina.org>
2019-06-27 10:13:10 -0400

    Inserção do Form para envio de Dados

--- web/index.php
+++ web/index.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link rel="stylesheet" href="css/index.css"> -->
-    <link rel="stylesheet" href="css/index2.css">
+    <link rel="stylesheet" href="css/index.css">
+    <!-- <link rel="stylesheet" href="css/index2.css"> -->
     <title>Example SQL Injection</title>
 </head>
+
 <body>
-<form action="login.php">
-<button type="submit">Enviar</button>
-</form>
+    <!-- <form action="login.php">
+<button class="button" type="submit">Enviar</button>
+</form> -->
     <div class="login">
         <h2 class="title">LOGIN</h2>
-        <input placeholder="user" type="text" class="input">
-        <input placeholder="Password" type="password" class="input">
-        <button class="button">Login</button>
+        <form action="login.php">
+            <input placeholder="user" type="text" class="input">
+            <input placeholder="Password" type="password" class="input">
+            <input type="submit" class="button" value="Login">
+        </form>
     </div>
 </body>
+
 </html>
